c303972071462293e87fee5ad3e86db09891f5c9
SatyamDubeyy <98874638+SatyamDubeyy@users.noreply.github.com>
2023-05-29 17:39:16 +0530

    Adding some more skills

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,18 +1,52 @@
-export const skills = [
+export const skills1 = [
   {
     name: "HTML",
-    iconClass: "devicon-html5-plain"
+    iconClass: "devicon-mongodb-plain"
   },
   {
     name: "CSS",
-    iconClass: "devicon-css3-plain"
+    iconClass: "devicon-express-original"
   },
   {
     name: "JavaScript",
-    iconClass: "devicon-javascript-plain"
+    iconClass: "devicon-react-plain"
   },
   {
     name: "React",
-    iconClass: "devicon-react-original"
+    iconClass: "devicon-nodejs-plain"
+  }
+]
+
+export const skills2 = [
+  {
+    name: "HTML",
+    iconClass: "devicon-sass-original"
+  },
+  {
+    name: "CSS",
+    iconClass: "devicon-bootstrap-plain"
+  },
+  // {
+  //   name: "PHASER",
+  //   iconClass: "devicon-react-plain"
+  // },
+  {
+    name: "ghsdf",
+    iconClass: "devicon-threejs-original"
+  }
+]
+
+export const skills3 = [
+  {
+    name: "HTML",
+    iconClass: " devicon-csharp-plain"
+  },
+  {
+    name: "CSS",
+    iconClass: " devicon-wordpress-plain"
+  },
+  {
+    name: "JavaScript",
+    iconClass: "devicon-unity-original"
   }
 ]

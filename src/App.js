178d2340698c79cac62c52798b8ea151b7f01c35
slimmwillis <Slimmwillis@gmail.com>
2023-06-03 08:23:19 -0400

    added contact details to Hire Me button and Message icon

--- src/App.js
+++ src/App.js
@@ -1,54 +1,70 @@
-import { useEffect, useState } from "react"
-import Header from "./components/header/Header"
-import Hero from "./components/hero_section/Hero"
-import Projects from "./components/projects/Projects"
-import Skills from "./components/skills/Skills"
-import "./index.css"
-import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
-import { Dialog, DialogTitle, stepButtonClasses } from "@mui/material"
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
-import {io} from "socket.io-client"
-import { BrowserRouter,Route,Link,Routes ,Switch,withRouter} from 'react-router-dom';
-import AboutMe from "./pages/AboutMe.js"
-import Contact from "./pages/Contact.js"
+import { useEffect, useState } from "react";
+import Header from "./components/header/Header";
+import Hero from "./components/hero_section/Hero";
+import Projects from "./components/projects/Projects";
+import Skills from "./components/skills/Skills";
+import "./index.css";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import { Dialog, DialogTitle, stepButtonClasses } from "@mui/material";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import { io } from "socket.io-client";
+import {
+  BrowserRouter,
+  Route,
+  Link,
+  Routes,
+  Switch,
+  withRouter,
+} from "react-router-dom";
+import AboutMe from "./pages/AboutMe.js";
+import Contact from "./pages/Contact.js";
 
 function App() {
   const [open, setOpen] = useState(false);
-  
-  useEffect(()=>{
-    const socket = io("http://localhost:3000")
-    socket.emit("uptome","information")
-  }, [])
 
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-  const handleOpen =()=>{
-    setOpen(true)
-}
-const handleClose =()=>{
-  setOpen(false)
-}
-
+  useEffect(() => {
+    const socket = io("http://localhost:3000");
+    socket.emit("uptome", "information");
+  }, []);
 
   return (
     <>
       <Header />
-      
-    <Routes>
-      <Route path={"/"} element={<><Hero />
-      <Skills />
-      <Projects />
-      <ChatBubbleOutlineOutlinedIcon onClick={handleOpen} sx={{ fontSize: 100, position: "fixed", bottom: 10, right: 10}}/>
 
-      <Dialog onClose={handleClose} open={open}>
-      <DialogTitle>Set backup account</DialogTitle>
-        hello world
-    </Dialog> </>}/>
-            <Route  path={'/Contact'} element={<Contact/>}/>
-            <Route  path={`/AboutMe`} element={<AboutMe/>}/>
+      <Routes>
+        <Route
+          path={"/"}
+          element={
+            <>
+              <Hero />
+              <Skills />
+              <Projects />
+              <ChatBubbleOutlineOutlinedIcon
+                onClick={handleOpen}
+                sx={{ fontSize: 100, position: "fixed", bottom: 10, right: 10 }}
+              />
+              <Dialog onClose={handleClose} open={open}>
+          <DialogTitle>Contact Informtaion</DialogTitle>
+          <h5 class="contactDetails">WhatsApp and Phone number</h5>
+          <p class="contactDetails">{`+1(904)800-5911`}</p>
+          <h5 class="contactDetails">Email</h5>
+          <p class="contactDetails">{`slimmwillis@gmail.com`}</p>
+        </Dialog>{" "}
+            </>
+          }
+        />
+        <Route path={"/Contact"} element={<Contact />} />
+        <Route path={`/AboutMe`} element={<AboutMe />} />
       </Routes>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/hero_section/Hero.js
+++ src/components/hero_section/Hero.js
@@ -1,22 +1,38 @@
 import React from "react";
 import TheSun from "./TheSun";
 import "./hero.css";
+import { useEffect, useState } from "react";
+import { Dialog, DialogTitle, stepButtonClasses } from "@mui/material";
 
 function Hero() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div className="hero" >
-      
+    <div className="hero">
       <TheSun />
 
       <div className="hero__content">
         <h2 className="hero__name">William Bailey</h2>
         <h3 className="hero__title">Web /WordPress /Game Developer</h3>
-
-        <button className="btn">Hire Me</button>
-
+        <Dialog onClose={handleClose} open={open}>
+          <DialogTitle>Contact Informtaion</DialogTitle>
+          <h5 class="contactDetails">WhatsApp and Phone number</h5>
+          <p class="contactDetails">{`+1(904)800-5911`}</p>
+          <h5 class="contactDetails">Email</h5>
+          <p class="contactDetails">{`slimmwillis@gmail.com`}</p>
+        </Dialog>{" "}
+        <button className="btn" onClick={handleOpen}>
+          Hire Me
+        </button>
       </div>
       {/* <div id="skills"> </div> */}
-     
     </div>
   );
 }

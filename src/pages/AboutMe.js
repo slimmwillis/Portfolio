71ddc8bbef63db151f0d823a052745223531e05e
slimmwillis <Slimmwillis@gmail.com>
2023-06-08 11:33:54 -0400

    Driving me crazyv1.sock

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,20 +1,40 @@
-import React from 'react'
-import "./aboutMe.css"
+import React from "react";
+import "./aboutMe.css";
+// import aboutMe from './aboutMe.jpg'
+
 export default function AboutMe() {
   return (
-    <div id='aboutMebg'>
-      <div className="aboutMeContainer">
-      <h1>William Bailey</h1>
-<h2>Full-Stack Web Developer</h2>
-<h3>MERN</h3>
-<h5>MongoDb, Express, React, Node</h5>
-<p>I am a full-stack web developer with a passion to solve complex problems.</p>
+    <div id="aboutMebg">
+      <div id="lavaPlanet">
+        <p id="lavaPlanetText">William Bailey</p>
+      </div>
 
-<h2>Game Developer</h2>
-<h3>Html5 Games & Unity</h3>
-<h5>PhaserJS, Three.JS, and Unity</h5>
-<p id='aboutMe'>As a boy I enjoyed games as the one time I get to live out fantasies and have fun not worry about getting hurt, judgement or even really making a lot of effort. Games are just an easy way to be happy and tap into your imagination. An escape from reality.</p>
-</div>
+      <div className="aboutMeContainer">
+        {/* <img src={aboutMe} className="aboutMeImage" alt="logo"/> */}
+
+        <h1 className="wordsAndStuff">William Bailey</h1>
+        <h2 className="wordsAndStuff">Full-Stack Web Developer</h2>
+        <h3 className="wordsAndStuff">MERN</h3>
+        <h5 className="wordsAndStuff">MongoDb, Express, React, Node</h5>
+        <p className="wordsAndStuff">
+          I am a full-stack web developer with a passion to solve complex
+          problems.
+        </p>
+
+        <h2 className="wordsAndStuff">Game Developer</h2>
+        <h3 className="wordsAndStuff">Html5 Games & Unity</h3>
+        <h5 className="wordsAndStuff">PhaserJS, Three.JS, and Unity</h5>
+        <p className="wordsAndStuff">
+          As a boy I enjoyed games as the one time I get to live out fantasies
+          and have fun not worry about getting hurt, judgement or even really
+          making a lot of effort. Games are just an easy way to be happy and tap
+          into your imagination. An escape from reality.
+        </p>
+        <p id="aboutMeHideMe" className="wordsAndStuff">
+          Also, lava planet is a div, not an image.
+        </p>
+      </div>
     </div>
-  )
+    //  </div>
+  );
 }
